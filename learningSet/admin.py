8b8f52ac491b9b7af9344b163ea9df9e0bdb443c
Polina Takhteeva <t-polya@mail.ru>
2018-03-16 18:59:56 +0300

    MySQL DataBase Connection

--- learningSet/admin.py
+++ learningSet/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from learningSet.models import Card
 
 # Register your models here.
+admin.site.register(Card)
--- learningSet/models.py
+++ learningSet/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+    created_date = models.DateTimeField('created date')
