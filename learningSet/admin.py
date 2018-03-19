8cfd55a16726034a51537871132f381fa5dd56b6
Polina Takhteeva <t-polya@mail.ru>
2018-03-19 18:32:03 +0300

    DataBase Model Creation

--- learningSet/admin.py
+++ learningSet/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from learningSet.models import Card
+from learningSet.models import Card, CardsSet, Comment, UserProfile
 
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(CardsSet)
+admin.site.register(Comment)
+admin.site.register(UserProfile)
--- learningSet/models.py
+++ learningSet/models.py
@@ -1,7 +1,42 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
 # Create your models here.
+
 class Card(models.Model):
-    question = models.CharField(max_length=200)
-    answer = models.CharField(max_length=200)
-    created_date = models.DateTimeField('created date')
+    question = models.CharField(max_length=1000)
+    answer = models.CharField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # class Meta:
+    #     verbose_name = 'Флеш-карта'
+    #     verbose_name_plural = 'Флеш-карта'
+
+class CardsSet(models.Model):
+	name = models.CharField(max_length=255)
+	educational_material = models.CharField(max_length = 10000)
+	created_at = models.DateTimeField(auto_now_add=True)
+    
+    # class Meta:
+    #    verbose_name = 'Набор флеш-карт'
+    #    verbose_name_plural = 'Наборы флеш-карт'
+
+class Comment(models.Model):
+	text = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+def get_image_path(instance, filename):
+		return os.path.join('photos', str(instance.id), filename)
+		
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+	specialty = models.CharField(max_length=255)
+
+
+  
+
+
