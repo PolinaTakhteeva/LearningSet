e6515e440075d41d529db95cb91045811e215805
Polina Takhteeva <t-polya@mail.ru>
2018-03-19 19:43:09 +0300

    Relations between tables

--- learningSet/admin.py
+++ learningSet/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from learningSet.models import Card, CardsSet, Comment, UserProfile
+from learningSet.models import Card, CardsSet, Comment, UserProfile, Favorite
 
 # Register your models here.
 admin.site.register(Card)
 admin.site.register(CardsSet)
 admin.site.register(Comment)
 admin.site.register(UserProfile)
+admin.site.register(Favorite)
+
--- learningSet/models.py
+++ learningSet/models.py
@@ -3,38 +3,42 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
-# Create your models here.
+class CardsSet(models.Model):
+	name = models.CharField(max_length=255)
+	educational_material = models.CharField(max_length = 10000)
+	created_at = models.DateTimeField(auto_now_add=True)
+	user = models.ManyToManyField(User)
+   
 
 class Card(models.Model):
     question = models.CharField(max_length=1000)
     answer = models.CharField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
-    # class Meta:
-    #     verbose_name = 'Флеш-карта'
-    #     verbose_name_plural = 'Флеш-карта'
+    cardsSet = models. ManyToManyField(CardsSet)
 
-class CardsSet(models.Model):
-	name = models.CharField(max_length=255)
-	educational_material = models.CharField(max_length = 10000)
-	created_at = models.DateTimeField(auto_now_add=True)
-    
-    # class Meta:
-    #    verbose_name = 'Набор флеш-карт'
-    #    verbose_name_plural = 'Наборы флеш-карт'
 
 class Comment(models.Model):
 	text = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
 
 
-
 def get_image_path(instance, filename):
-		return os.path.join('photos', str(instance.id), filename)
+	return os.path.join('photos', str(instance.id), filename)
 		
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 	specialty = models.CharField(max_length=255)
+
+# Many to many tables 
+
+class Favorite(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE);
+	CardsSet = models.OneToOneField(CardsSet, on_delete=models.CASCADE)
+
+class TestGroup(models.Model):
+	parentCardsSet = models.ForeignKey(CardsSet, on_delete=models.CASCADE, related_name='TestGroup.parentCardsSet+')
+	childCardsSet = models.ForeignKey(CardsSet, on_delete=models.CASCADE, related_name='TestGroup.childCardsSet+')
 
 
   
