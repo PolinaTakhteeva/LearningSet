3d0c3f8ce87ead14e7c65e644417b0aa5a51b1f6
Polina Takhteeva <t-polya@mail.ru>
2018-03-26 22:47:11 +0300

    Create user views

--- application/urls.py
+++ application/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from learningSet import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/list/', views.users_list, name='users_list'),
+    path(
+        'users/<int:user_id>/',
+        views.user_detail, name='user_detail')
 ]
--- learningSet/views.py
+++ learningSet/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.contrib.auth.models import User
+from learningSet.models import Card, CardsSet
 
-# Create your views here.
+def users_list(request):
+	users = User.objects.all()[:20]
+	return render(
+		request, 'learningSet/users_list.html',
+		{'users': users}
+		)
+
+def user_detail(request, user_id):
+	try:
+		user = User.objects.get(id=user_id)
+	except User.DoesNotExist:
+		raise Http404
+	return render(
+		request, 'learningSet/user_detail.html',
+		{'user': user}
+		)
+
+def cardsSets_list(request):
+	sets = CardsSet.objects.all()[:10]
+	return render(
+		request, 'learningSet/users_list.html',
+		{'sets': sets}
+		)	
+
+def my_cardsSets_list(request):
+	my_sets = CardsSet.objects.all()[:10]
+
+
+def cardSet(request):
+	set = Card. object.all()[:5]
+	return render(
+		request, 'learningSet/cardSet_detail.html',
+		)
+
+
+
+def card(request, pk):
+      try:
+          object = Card.objects.get(pk=pk)
+      except Post.DoesNotExist:
+          raise Http404
+      return render(
+           request, 'blog/post_detail.html',
+           {'object': object}
+      )
