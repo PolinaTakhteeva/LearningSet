ad316e9079b2c145e1e84c430ca23b862f9eae7d
Polina Takhteeva <t-polya@mail.ru>
2018-03-26 13:53:58 +0300

    Add models class Meta

--- learningSet/models.py
+++ learningSet/models.py
@@ -15,13 +15,23 @@ class Comment(models.Model):
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey('content_type', 'object_id')
 
+	class Meta:
+		verbose_name = 'Комментарий'
+		verbose_name_plural = 'Комментарии'
+
 
 class CardsSet(models.Model): #group with rights
 	name = models.CharField(max_length=255, db_index=True) #unic control  by data base
-	educational_material = models.TextField()
+	description = models.TextField(null=True)
+	educational_material = models.TextField(null=True)
 	created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-	creator = models.ForeignKey(User, on_delete=models.CASCADE);
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
 	comments = GenericRelation(Comment)
+
+	class Meta:
+		ordering = ('created_at',)
+		verbose_name = 'Набор тестовых карт'
+		verbose_name_plural = 'Наборы тестовых карт'
    
 
 class Card(models.Model):
@@ -31,12 +41,19 @@ class Card(models.Model):
     cardsSet = models. ManyToManyField(CardsSet)
     comments = GenericRelation(Comment)
 
+    class Meta:
+        verbose_name = 'Флеш-карта'
+        verbose_name_plural = 'Флеш-карты'
 
 # Many to many tables 
 
 class Favorite(models.Model): #fk
 	user = models.ForeignKey(User, on_delete=models.CASCADE);
 	CardsSet = models.ForeignKey(CardsSet, on_delete=models.CASCADE)
+	
+	class Meta:
+		verbose_name = 'Избранное'
+		verbose_name_plural = 'Избранное'
 
 class TestGroup(models.Model):
 	parentCardsSet = models.ForeignKey(CardsSet, on_delete=models.CASCADE, related_name='TestGroup.parentCardsSet+')
